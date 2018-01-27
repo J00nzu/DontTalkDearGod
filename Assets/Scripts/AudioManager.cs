ea3f94b351e3e9386bb078bef600eec0e6bd2561
Amir Ingher <amir.ingher26@gmail.com>
2018-01-27 22:23:11 +0200

    AudioManager,Sounds,scene audio test, audio clips

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound[] sounds;
+	// Use this for initialization
+
+
+	void Awake(){
+
+		foreach (Sound s in sounds) {
+			s.source = gameObject.AddComponent<AudioSource> ();
+
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+
+			s.source.pitch = s.pitch;
+
+			s.source.loop = s.loop;
+
+		}
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Play(string name){
+		Sound sound = new Sound();
+		foreach (Sound s in sounds) {
+
+			if (s.name.Equals (name)) {
+
+
+				sound = s;
+			}
+
+
+		}
+		if (sound == null) {
+			return;
+
+		}
+		sound.source.Play ();
+	}
+}
