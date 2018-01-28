578f97348a539b82d492744e903387dfae579954
Amir Ingher <amir.ingher26@gmail.com>
2018-01-28 16:59:36 +0200

    da end

---
+++ Assets/Scripts/EndVideoAndLeave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+using UnityEngine.SceneManagement;
+
+public class EndVideoAndLeave : MonoBehaviour {
+	private VideoPlayer vp;
+	// Use this for initialization
+	void Start () {
+		vp = this.gameObject.GetComponent<VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!vp.isPlaying) {
+			SceneManager.LoadScene (0);
+		}
+	}
+}
