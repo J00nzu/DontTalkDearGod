53c167ce065384d2bce9ae6a8b54319f1ed981d6
x-EricH-x <erich.contact.me@gmail.com>
2018-01-28 17:06:38 +0200

    added some work in progress test intro scene and a simple text fade in intro scene

--- Assets/Scripts/CamTweeningScript.cs
+++ Assets/Scripts/CamTweeningScript.cs
@@ -1,35 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class CamTweeningScript : MonoBehaviour {
 	public float animationTimeSec = 2.5f;
 	//[Range(0.0f, 1.0f)] public float parameter = 0.0f;
 	//public float parameter = 0.0f;
-	public float parameter;
+	public float parameter = 0.0f;
+
+	public float IntroStaticTextHoldSec = 2.0f;
+	public float IntroStaticTextSec = 5.5f;
+
+	bool introTextDone = false;
+	Image img01;
+	float t, clamped_t;
+	Color lerpedColor = Color.clear;
 
 	float animationTimeLapsed = 0.0f;
 	BansheeGz.BGSpline.Components.BGCcCursor camPosCursor, camRotCursor;
 
 	// Use this for initialization
 	void Start () {
-		if (parameter == null) parameter = 0.8f;
 		camPosCursor = transform.Find ("BGCurve CamPos").gameObject.GetComponent<BansheeGz.BGSpline.Components.BGCcCursor> ();
 		camRotCursor = transform.Find ("BGCurve CamRot").gameObject.GetComponent<BansheeGz.BGSpline.Components.BGCcCursor> ();
+
+		img01 = transform.Find ("Canvas").Find ("Image").GetComponent<Image>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		animationTimeLapsed += Time.deltaTime;
-		if (animationTimeLapsed < animationTimeSec && parameter < 1.0f) {
-			parameter += Time.deltaTime / animationTimeSec;
+		if (!introTextDone) {
+			t = Time.time;
+			if (t > IntroStaticTextHoldSec) {
+				clamped_t = Mathf.Clamp ((t - IntroStaticTextHoldSec) / (IntroStaticTextSec - IntroStaticTextHoldSec), 
+					                     0.0f, 1.0f);
+					
+				lerpedColor = Color.Lerp (Color.white, Color.clear, clamped_t);
+				img01.color = lerpedColor;
+			}
+			if (t > IntroStaticTextSec)	introTextDone = true;
+		}
 
-			camPosCursor.DistanceRatio = EasingFunction.EaseInOutSine (0.0f, 1.0f, parameter);
-			//camRotCursor.DistanceRatio = EasingFunction.EaseInOutQuad (0.0f, 1.0f, animationTimeLapsed / animationTimeSec);
-			camRotCursor.DistanceRatio = camPosCursor.DistanceRatio;
-		} else {
-			parameter = 1.0f;
+
+		//if (introTextDone) {
+		if (Time.time > IntroStaticTextHoldSec) {
+			animationTimeLapsed += Time.deltaTime;
+			if (animationTimeLapsed < animationTimeSec && parameter < 1.0f) {
+				parameter += Time.deltaTime / animationTimeSec;
+
+				camPosCursor.DistanceRatio = EasingFunction.EaseInOutSine (0.0f, 1.0f, parameter);
+				//camRotCursor.DistanceRatio = EasingFunction.EaseInOutQuad (0.0f, 1.0f, animationTimeLapsed / animationTimeSec);
+				camRotCursor.DistanceRatio = camPosCursor.DistanceRatio;
+			} else {
+				parameter = 1.0f;
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/IntroSeqScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroSeqScript : MonoBehaviour {
+	public float[] keys = {
+		1.0f, 2.5f,
+		3.5f, 4.5f,
+		5.5f, 6.5f,
+	};
+
+	public int keyPairIdx = 0;
+	float t = 0.0f;
+
+	Color lerpedColor = Color.clear;
+	Image img01,img02,img03;
+
+	// Use this for initialization
+	void Start () {
+		img01 = transform.Find ("Image donttalk").GetComponent<Image> ();
+		img02 = transform.Find ("Image ohdeargod").GetComponent<Image> ();
+		img03 = transform.Find ("Image end").GetComponent<Image> ();
+
+		InvokeRepeating("fadeImg01", keys[0], 0.02f);
+	}
+
+	void fadeImg01 () {
+		t = Time.time;
+		keyPairIdx = 0;
+
+		lerpedColor = Color.Lerp (Color.black, Color.white, 
+			Mathf.Clamp ( (t - keys[keyPairIdx]) / (keys[keyPairIdx] - keys[keyPairIdx+1]), 
+			               0.0f, 1.0f));
+		img01.color = lerpedColor;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
